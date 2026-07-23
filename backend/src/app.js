4236f44be0ae08823a77aed1d8cb87a19d27f51f
riturajlabs <riturajlabs@outlook.com>
2026-07-23 12:28:20 +0530

    Update production API URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,11 +33,16 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
+
     if (!origin) return callback(null, true);
+
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
-    return callback(new Error('Not allowed by CORS'));
+
+    console.log('Blocked CORS Origin:', origin);
+
+    return callback(null, false);
   },
   credentials: true,
 }));
